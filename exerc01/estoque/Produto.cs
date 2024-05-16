18462838f6de9e79570d53b3571fd3b1d8d96948
Enzo Caetano P. Rodrigues <124791449+EnzoCaetano015@users.noreply.github.com>
2024-05-16 19:52:00 -0300

    Add files via upload

---
+++ exerc01/estoque/Produto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace estoque
+{
+    class Produto
+    {
+        // Atributos
+        private string nome;
+        private double preco;
+        private int quantidadeEmEstoque;
+
+        // Construtor
+        public Produto(string nome, double preco, int quantidadeEmEstoque)
+        {
+            this.nome = nome;
+            this.preco = preco;
+            this.quantidadeEmEstoque = quantidadeEmEstoque;
+        }
+
+        // Método para adicionar itens ao estoque
+        public void AdicionarItemEstoque(int quantidade)
+        {
+            if (quantidade > 0)
+            {
+                quantidadeEmEstoque += quantidade;
+                Console.WriteLine($"{quantidade} {nome}(s) adicionado(s) ao estoque.");
+            }
+            else
+            {
+                Console.WriteLine("Quantidade inválida. Nenhum item adicionado ao estoque.");
+            }
+        }
+
+        // Método para remover itens do estoque
+        public void RemoverItemEstoque(int quantidade)
+        {
+            if (quantidade > 0 && quantidade <= quantidadeEmEstoque)
+            {
+                quantidadeEmEstoque -= quantidade;
+                Console.WriteLine($"{quantidade} {nome}(s) removido(s) do estoque.");
+            }
+            else
+            {
+                Console.WriteLine("Quantidade inválida. Nenhum item removido do estoque.");
+            }
+        }
+
+        // Método para exibir informações do produto
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine($"Nome: {nome}");
+            Console.WriteLine($"Preço: R${preco:F2}");
+            Console.WriteLine($"Quantidade em Estoque: {quantidadeEmEstoque}");
+        }
+    }
+}
---
+++ exerc01/estoque/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace estoque
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exemplo de uso da classe Produto
+            Produto produto1 = new Produto("Caneta", 2.50, 10);
+            produto1.ExibirInformacoes();
+
+            produto1.AdicionarItemEstoque(5);
+            produto1.ExibirInformacoes();
+
+            produto1.RemoverItemEstoque(3);
+            produto1.ExibirInformacoes();
+        }
+    }
+}
